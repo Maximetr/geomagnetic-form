ab08596cfd2b5b5c874cabfc97ce618c7c52fabf
Дмитрий Пантелеев <d.panteleev@gcras.local>
2018-01-17 13:19:45 +0300

    css fix

--- index.php
+++ index.php
@@ -13,115 +13,95 @@ session_start();
 </head>
 <body>
 
-    <div class = "mainbody clearfix">
-        <div class="map" id="mapid" style="width: 855.58px; height: 433px"></div>
+    <div class = "mainbody">
+        <div class = "content clearfix">
+            <div class="map" id="mapid" style="width: 855.58px; height: 433px"></div>
         
-        
-    <div class = "form">
-        <form method="post" action="index.php">
-                <div class = "datatype">
-                    <div class = "head">
+
+            <div class = "form">
+                <form method="post" action="index.php">
+                        <div class = "datatype">
                         <h3>Вид данных</h3>
+                                <label class="ntSaveForms">
+                                    <input type="radio" name="typedata" class="ntSaveForms" value="3" checked />Среднечасовые
+                                </label>
+                        </div>
+                    <div class = "observatory">
+                        <h3>Обсерватория</h3>
+                            <p>Название обсерватории</p>
+                            <select  id="obsnametab" name="obsnametab" onchange="onSelectChange()">
+                                <option value = "AAA">AAA, Alma-Ata</option>
+                                <option value = "AIA">AIA, Argentine Islands</option>
+                                <option value = "ARK">ARK, Arkhangelsk</option>
+                                <option value = "ASH">ASH, Ashkhabad</option>
+                                <option value = "BOX">BOX, Borok</option>
+                                <option value = "CSS">CSS, Cape Chelyuskin</option>
+                                <option value = "CWE">CWE, Cape  Wellen (Uelen)</option>
+                                <option value = "DIK">DIK, Dixon (Dikson Island)</option>
+                                <option value = "HIS">HIS, Heiss Island (Druzhnaya)</option>
+                                <option value = "IRT">IRT, Irkutsk (Patrony)</option>
+                                <option value = "KGD">KGD, Karaganda</option>
+                                <option value = "KIV">KIV, Kiev</option>
+                                <option value = "KZN">KZN, Kazan</option>
+                                <option value = "LNN">LNN, Voyekovo (Leningrad)</option>
+                                <option value = "LVV">LVV, Lvov</option>
+                                <option value = "MGD">MGD, Magadan</option>
+                                <option value = "MOS">MOS, Moscow</option>
+                                <option value = "NVS">NVS, Novosibirsk</option>
+                                <option value = "PET">PET, Paratunka (Petropavlovsk)</option>
+                            </select>
                     </div>
-                    <div class = "hour">
-                        <label class="ntSaveForms">
-                            <input type="radio" name="typedata" class="ntSaveForms" value="3" checked />Среднечасовые
-                        </label>
-                    </div>
-                </div>
-            <div class = "observatory">
-                <div class = "head">
-                    <h3>Обсерватория</h3>
-                </div>
-                <div class = "obsselect">
-                    <p>Название обсерватории</p>
-                    <select  id="obsnametab" name="obsnametab" onchange="onSelectChange()">
-                        <option value = "AAA">AAA, Alma-Ata</option>
-                        <option value = "AIA">AIA, Argentine Islands</option>
-                        <option value = "ARK">ARK, Arkhangelsk</option>
-                        <option value = "ASH">ASH, Ashkhabad</option>
-                        <option value = "BOX">BOX, Borok</option>
-                        <option value = "CSS">CSS, Cape Chelyuskin</option>
-                        <option value = "CWE">CWE, Cape  Wellen (Uelen)</option>
-                        <option value = "DIK">DIK, Dixon (Dikson Island)</option>
-                        <option value = "HIS">HIS, Heiss Island (Druzhnaya)</option>
-                        <option value = "IRT">IRT, Irkutsk (Patrony)</option>
-                        <option value = "KGD">KGD, Karaganda</option>
-                        <option value = "KIV">KIV, Kiev</option>
-                        <option value = "KZN">KZN, Kazan</option>
-                        <option value = "LNN">LNN, Voyekovo (Leningrad)</option>
-                        <option value = "LVV">LVV, Lvov</option>
-                        <option value = "MGD">MGD, Magadan</option>
-                        <option value = "MOS">MOS, Moscow</option>
-                        <option value = "NVS">NVS, Novosibirsk</option>
-                        <option value = "PET">PET, Paratunka (Petropavlovsk)</option>
-                    </select>
-                </div>
-            </div>
 
-            <div class = "timeselect">
-                <div class = "head">
-                    <h3>Временной интервал</h3>
-                </div>
-                <div class = "mintime">
-                    <lable>От
-                        <input type="text" name="date1" id="date1" class="ntSaveForms" placeholder="YYYY-MM-DD" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"  size="10">                
-                    </lablel>
-                </div>
-                <div class = "maxtime">
-                    <lable>до
-                        <input type="text" name="date2" id="date2" class="ntSaveForms" placeholder="YYYY-MM-DD" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"  size="10">
-                    </label>
-                </div>
-            </div>
+                    <div class = "timeselect">
+                        <h3>Временной интервал</h3>
+                        <lable>От
+                            <input type="text" name="date1" id="date1" class="ntSaveForms" placeholder="YYYY-MM-DD" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"  size="10">                
+                        </lablel>
+                        <lable>до
+                            <input type="text" name="date2" id="date2" class="ntSaveForms" placeholder="YYYY-MM-DD" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"  size="10">
+                        </label>
+                    </div>
 
-            <div class = "output">
-                <div class = "head">
-                    <h3>Формат вывода данных</h3>
-                </div>
-                <div class = "formats">
-                    <div class = "WDC">
-                        <label>
-                            <input type="radio" name="savedata" class="ntSaveForms" value="WDC" checked />WDC
-                        </label>
+                    <div class = "output">
+                        <h3>Формат вывода данных</h3>
+                            <label>
+                                <input type="radio" name="savedata" class="ntSaveForms" value="WDC" checked />WDC
+                            </label>
+                            <label>
+                                <input type="radio" name="savedata" class="ntSaveForms" value="CSV" />CSV
+                            </label>
+                            <label>
+                                <input type="radio" name="savedata" class="ntSaveForms" value="IAGA2002" />IAGA2002
+                            </label></br>
+                            <input class="button" type="submit" value="Найти" name="submit1" id="" />
                     </div>
-                    <div class = "CSV">
-                        <label>
-                            <input type="radio" name="savedata" class="ntSaveForms" value="CSV" />CSV
-                        </label>
-                    </div>
-                    <div class = "IAGA2002">
-                        <label>
-                            <input type="radio" name="savedata" class="ntSaveForms" value="IAGA2002" />IAGA2002
-                        </label>
-                    </div>
-                    <div class = "submit">
-                        <input type="submit" value="Найти" name="submit1" id="" />
-                    </div><?php if (isset($_POST['submit1'])) : ?>
-                    <textarea><?php
-                        /* error_reporting(E_ALL);
-                        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); */
-                        require_once('components/connect.php');
-                        $iagaKod = $_REQUEST['obsnametab'];
-                        $mindate = $_REQUEST['date1'];
-                        $maxdate = $_REQUEST['date2'];
-                        $format = $_REQUEST['savedata'];
-                        if ($format == 'WDC') {
-                            include_once('models/wdcb.php');
-                        }
-                        if ($format == 'CSV') {
-                            include_once('models/csv.php');
-                        }
-                        if ($format == 'IAGA2002') {
-                            include_once('models/iaga.php');
-                        }
-
-                        ?></textarea>
-                        <?php endif; ?>
-                </div>
             </div>
-        </form>
-    </div>    
+                            <?php if (isset($_POST['submit1'])) : ?>
+                            <textarea><?php
+                                /* error_reporting(E_ALL);
+                                mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); */
+                                require_once('components/connect.php');
+                                $iagaKod = $_REQUEST['obsnametab'];
+                                $mindate = $_REQUEST['date1'];
+                                $maxdate = $_REQUEST['date2'];
+                                $format = $_REQUEST['savedata'];
+                                if ($format == 'WDC') {
+                                    include_once('models/wdcb.php');
+                                }
+                                if ($format == 'CSV') {
+                                    include_once('models/csv.php');
+                                }
+                                if ($format == 'IAGA2002') {
+                                    include_once('models/iaga.php');
+                                }
+
+                                ?></textarea>
+                                <?php endif; ?>
+
+                    
+                </form>
+            
+        </div>        
     <script type='text/javascript' src='ntsaveforms.js'></script>
     </div>
     <script src="leaflet/leaflet.js"></script>
